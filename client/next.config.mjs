70b616405b28c9563eea24c8b797d0ff946c03a0
tuanminh.nguyen@student.adelaide.edu.au <tuanminh.nguyen@student.adelaide.edu.au>
2025-02-04 21:27:05 +1030

    final

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://s3-inventory-management-andy.s3.ap-southeast-2.amazonaws.com",
+        hostname: "s3-inventory-management-andy.s3.ap-southeast-2.amazonaws.com",
         port: "",
         pathname: "/**",
       },

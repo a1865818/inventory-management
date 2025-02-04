889a76b9eb9ce27fa8ec8aa42e4349b88dc900e2
tuanminh.nguyen@student.adelaide.edu.au <tuanminh.nguyen@student.adelaide.edu.au>
2025-02-04 18:07:45 +1030

    try to fix

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -26,15 +26,9 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 /* REDUX PERSISTENCE */
 const createNoopStorage = () => {
   return {
-    getItem(_key: any) {
-      return Promise.resolve(null);
-    },
-    setItem(_key: any, value: any) {
-      return Promise.resolve(value);
-    },
-    removeItem(_key: any) {
-      return Promise.resolve();
-    },
+    getItem: async (_key: string): Promise<null> => null, // `_key` is ignored
+    setItem: async (_key: string, value: unknown): Promise<unknown> => value, // `_key` is ignored
+    removeItem: async (_key: string): Promise<void> => {}, // `_key` is ignored
   };
 };
 
